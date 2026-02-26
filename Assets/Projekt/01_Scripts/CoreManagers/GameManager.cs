8e1e7ccb7f4b20427e42ca1d584545ca031ca000
156Sheong <waladeus@gmail.com>
2026-02-26 11:08:10 +0900

    게임 매니저 추가

---
+++ Assets/Projekt/01_Scripts/CoreManagers/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    public List<GameObject> coreManagers = new List<GameObject>();
+
+
+
+
+    // Awake 를 사용할거면 protected override void Awake() 형태로 해야함
+
+
+    // 임시로 초기에 씬 넘어가는 기능 구현
+    private void Start()
+    {
+        Init();
+
+    }
+
+
+    private void Init()
+    {
+        // 각종 매니저 생성
+        foreach (GameObject manager in coreManagers)
+        {
+            if (manager != null)
+            {
+                Instantiate(manager);
+            }
+        }
+    }
+
+}
